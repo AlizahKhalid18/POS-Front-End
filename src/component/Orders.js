eca74802b9a4aa14f63af10ceaf18857439cbfa0
Alizah Khalid <khalidaliza237@gmail.com>
2024-07-28 17:36:39 +0500

    fix

--- src/component/Orders.js
+++ src/component/Orders.js
@@ -1,30 +1,81 @@
 import { Table } from "antd";
-function Orders(){
-    const dataSource = [
+import axios from "axios";
+import { useEffect, useState } from "react";
+function Orders() {
+  const [orders, setOrders] = useState([]);
 
-    ];
-    
-    const columns = [
-      {
-        title: 'Name',
-        dataIndex: 'name',
-        key: 'name',
+  const columns = [
+    {
+      title: "Order ID",
+      dataIndex: "_id",
+      key: "_id",
+    },
+    {
+      title: "Total",
+      dataIndex: "total",
+      key: "total",
+    },
+  ];
+
+  const itemColumns = [
+    {
+      title: "Product Name",
+      dataIndex: "productName",
+      render: (_, record) => {
+        return <> {record?.product?.productName} </>;
       },
-      {
-        title: 'Age',
-        dataIndex: 'age',
-        key: 'age',
+    },
+    {
+      title: "Product Price",
+      dataIndex: "productPrice",
+      render: (_, record) => {
+        return <> {record?.product?.productPrice} </>;
       },
-      {
-        title: 'Address',
-        dataIndex: 'address',
-        key: 'address',
+    },
+    {
+      title: "Product Quantity",
+      dataIndex: "quantity",
+      render: (_, record) => {
+        return <> {record?.quantity} </>;
       },
-    ];
-    return(
-<div>
-<Table dataSource={dataSource} columns={columns} />;
-</div>
-    );
+    },
+    {
+      title: "Product Total Price",
+      dataIndex: "productTotalPrice",
+      render: (_, record) => {
+        return <> {record?.quantity * record?.product?.productPrice} </>;
+      },
+    },
+  ];
+
+  async function getOrders() {
+    const orders = await axios.get("http://localhost:5000/orders");
+    setOrders(orders.data.orders);
+  }
+
+  useEffect(() => {
+    getOrders();
+  }, []);
+
+  return (
+    <div>
+      <Table
+        expandable={{
+          expandedRowRender: (record) => {
+            return (
+              <Table
+                pagination={false}
+                dataSource={record.items}
+                columns={itemColumns}
+              />
+            );
+          },
+        }}
+        dataSource={orders}
+        columns={columns}
+        rowKey={(record) => record._id}
+      />
+    </div>
+  );
 }
 export default Orders;
--- src/component/Pos.js
+++ src/component/Pos.js
@@ -7,23 +7,32 @@ import { Collapse, Card, List, Input } from "antd";
 import "./Pos.css";
 import axios from "axios";
 import { ShoppingCartOutlined } from "@ant-design/icons";
+import { toast, ToastContainer } from "react-toastify";
 const { Meta } = Card;
 
-const AddToCartButton = () => {
+const AddToCartButton = ({ addToCart, record }) => {
   return (
-    <Row>
-      <Col>Add To Cart</Col>
-      <Col>
-        {" "}
-        <ShoppingCartOutlined key="cart" />{" "}
-      </Col>
-    </Row>
+    <button
+      onClick={() => addToCart({ ...record, quantity: 1 })}
+      className="w-100 addToCartBtn"
+    >
+      <Row>
+        <Col lg={9} className="d-flex">
+          Add To Cart
+        </Col>
+        <Col lg={3}>
+          {" "}
+          <ShoppingCartOutlined key="cart" />{" "}
+        </Col>
+      </Row>
+    </button>
   );
 };
 
 function Pos() {
   const [listItems, setListItem] = useState([]);
   const [search, setSearch] = useState("");
+  const [cartList, setCartList] = useState([]);
 
   function handleSearchChange(event) {
     setSearch(event.target.value);
@@ -32,7 +41,7 @@ function Pos() {
   async function handleSimpleSearch() {
     const body = {
       search: search,
-    }
+    };
     const result = await axios.post(
       "http://localhost:5000/products/search",
       body
@@ -48,6 +57,54 @@ function Pos() {
   useEffect(() => {
     getPageData();
   }, []);
+
+  const addToCart = (cartItem) => {
+    let _cartList = [...cartList];
+    const foundItem = _cartList.filter((item) => item._id === cartItem._id);
+    if (foundItem.length) {
+      return;
+    }
+    _cartList.push(cartItem);
+    setCartList(_cartList);
+  };
+
+  const handleQauntity = (opr, record) => {
+    let _cartList = [...cartList];
+    const index = _cartList.findIndex((x) => x._id === record._id);
+    if (opr === "+") {
+      if (index > -1) {
+        _cartList[index].quantity += 1;
+        setCartList(_cartList);
+      }
+    } else {
+      if (index > -1 && _cartList[index].quantity > 1) {
+        _cartList[index].quantity -= 1;
+        setCartList(_cartList);
+      }
+    }
+  };
+
+  const handleRemoveItem = (item) => {
+    let _cartList = [...cartList];
+    const index = _cartList.findIndex((x) => x._id === item._id);
+    _cartList.splice(index, 1);
+    setCartList(_cartList);
+  };
+
+  const placeOrder = async () => {
+    const payload = {
+      items: cartList.map((x) => ({ product: x._id, quantity: x.quantity })),
+      total: cartList.reduce(
+        (acc, curr) => acc + curr.productPrice * curr.quantity,
+        0
+      ),
+    };
+    const result = await axios.post("http://localhost:5000/orders", payload);
+    if (result.data.status === "ok") {
+      toast(result.data.message);
+      setCartList([]);
+    }
+  };
 
   const items = [
     {
@@ -121,49 +178,122 @@ function Pos() {
   ];
   return (
     <div className="container-fluid">
-      <div className="col-8">
-        <Collapse items={items} />
-        <div className="mt-5 overflow-auto overflow-x-hidden listContainter">
-          <Row className="p-3">
-            <Row>
-              <Col>
-                <h5> Products </h5>
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <List
-                  grid={{
-                    gutter: 16,
-                    column: 4,
-                  }}
-                  dataSource={listItems}
-                  renderItem={(item) => (
-                    <List.Item>
-                      <Card
-                        cover={
-                          <img
-                            src={item.productImage}
-                            height={100}
-                            width={100}
+      <div className="row">
+        <div className="col-7">
+          <Collapse items={items} />
+          <div className="mt-5 overflow-auto overflow-x-hidden listContainter">
+            <Row className="p-3">
+              <Row>
+                <Col>
+                  <h5> Products </h5>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <List
+                    grid={{
+                      gutter: 16,
+                      column: 4,
+                    }}
+                    dataSource={listItems}
+                    renderItem={(item) => (
+                      <List.Item>
+                        <Card
+                          cover={
+                            <img
+                              src={item.productImage}
+                              height={100}
+                              width={100}
+                            />
+                          }
+                          actions={[
+                            <AddToCartButton
+                              addToCart={addToCart}
+                              record={item}
+                            />,
+                          ]}
+                        >
+                          <Meta
+                            title={item.productName}
+                            description={`${item.productPrice} RS`}
                           />
-                        }
-                        actions={[<AddToCartButton />]}
+                        </Card>
+                      </List.Item>
+                    )}
+                  />
+                </Col>
+              </Row>
+            </Row>
+          </div>
+        </div>
+        <div className="col-5 cart">
+          <Row className="p-3">
+            <h3 className="p-0"> Cart </h3>
+            <table>
+              <thead>
+                <th>Product Name</th>
+                <th>Product Price</th>
+                <th>Product Total Price</th>
+                <th>Quantity</th>
+                <th>Action</th>
+              </thead>
+
+              <tbody>
+                {cartList.map((item) => (
+                  <tr style={{ height: "40px" }} key={item._id}>
+                    <td> {item.productName} </td>
+                    <td> {item.productPrice} </td>
+                    <td> {item.productPrice * item.quantity} </td>
+                    <td>
+                      <button
+                        onClick={() => handleQauntity("-", item)}
+                        className="quntBtn "
                       >
-                        <Meta
-                          title={item.productName}
-                          description={`${item.productPrice} RS`}
-                        />
-                      </Card>
-                    </List.Item>
-                  )}
-                />
+                        -
+                      </button>
+                      {"  "} {item.quantity} {"  "}
+                      <button
+                        onClick={() => handleQauntity("+", item)}
+                        className="quntBtn"
+                      >
+                        +
+                      </button>
+                    </td>
+                    <td>
+                      <button
+                        className="quntBtn"
+                        onClick={() => handleRemoveItem(item)}
+                      >
+                        {" "}
+                        Remove{" "}
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </Row>
+          <Row>
+            <hr />
+            <Row>
+              <Col lg={10}>Total price</Col>
+              <Col lg={2}>
+                {cartList.reduce(function (acc, obj) {
+                  return acc + obj.productPrice * obj.quantity;
+                }, 0)}{" "}
+                RS/.
               </Col>
             </Row>
           </Row>
+          <Row className="p-2">
+            <button className="quntBtn" onClick={placeOrder}>
+              {" "}
+              Place Order{" "}
+            </button>
+          </Row>
         </div>
       </div>
-      <div className="col-4"></div>
+      <ToastContainer />
     </div>
   );
 }
